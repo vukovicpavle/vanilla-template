c9e9ee67c2c10adc2afc9280485f01de6d961dc6
Pavle Vuković <vukovic.pavle@icloud.com>
2023-03-25 11:29:29 +0100

    add image and effects; cleanup js

--- src/index.js
+++ src/index.js
@@ -5,40 +5,36 @@ function initializeTheme() {
   } else {
     setTheme("system");
   }
-
-  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-  // if (
-  //   localStorage.theme === "dark" ||
-  //   (!("theme" in localStorage) &&
-  //     window.matchMedia("(prefers-color-scheme: dark)").matches)
-  // ) {
-  //   document.documentElement.classList.add("dark");
-  // } else {
-  //   document.documentElement.classList.remove("dark");
-  // }
 }
 
 function setTheme(theme) {
   switch (theme) {
     case "dark":
-      document.documentElement.classList.add("dark");
-      localStorage.theme = "dark";
+      setDarkTheme();
       break;
     case "light":
-      document.documentElement.classList.remove("dark");
-      localStorage.theme = "light";
+      setLightTheme();
       break;
     default:
-      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-      }
-      localStorage.removeItem("theme");
+      setDefaultTheme();
   }
 }
 
-// On page load
-// document.addEventListener("DOMContentLoaded", () => {
-//   initializeTheme();
-// });
+function setDarkTheme() {
+  document.documentElement.classList.add("dark");
+  localStorage.theme = "dark";
+}
+
+function setLightTheme() {
+  document.documentElement.classList.remove("dark");
+  localStorage.theme = "light";
+}
+
+function setDefaultTheme() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+  localStorage.removeItem("theme");
+}
